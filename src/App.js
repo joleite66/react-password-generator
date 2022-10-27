0d198141acf78ac30bdd31b43c4252c630879176
João Leite <joleite66@gmail.com>
2022-10-27 14:56:49 +0100

    adding Checkbox component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Checkbox from "./components/Checkbox";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div></div>
+      
     </div>
   );
 }
